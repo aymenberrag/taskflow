ddaad67d5591547c19e43bb76475fdc35083a361
aymen <aymenberrag2017@gmail.com>
2026-06-08 16:12:51 +0100

    change config

--- flask/app/config.py
+++ flask/app/config.py
@@ -1,5 +1,10 @@
+import os
+
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///project_manager.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = "aymen"
+    JWT_SECRET_KEY = os.getenv(
+        "JWT_SECRET_KEY",
+        "dev-secret-key"
+    )
